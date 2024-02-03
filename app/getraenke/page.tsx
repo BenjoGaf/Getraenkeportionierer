d038807ce18e1526280adea015cb234419a10e9f
BenjoGaf <benjamin.gangol@gmail.com>
2024-02-03 19:06:11 +0100

    db

--- app/getraenke/page.tsx
+++ app/getraenke/page.tsx
@@ -3,15 +3,26 @@ const Drink = require("@/components/Drink");
 
 mongoose.connect("mongodb://localhost/getraenkeliste");
 
+let Error = "";
+
+function drinkAlreadyExists() {
+  Error = "Getränk wurde schon hinzugefügt";
+}
+
+
 async function newDrink(name: string) {
+  const tryName = await Drink.find({drinkName: name})
+
+  if(tryName === /* [] */) {
   const newDrink = await Drink.create({ drinkName: name, pumpe: null });
   console.log(newDrink);
+  } else drinkAlreadyExists();
 }
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-around items-center justify-items-start p-12 h-screen">
-      <div>()</div>
+      (Drink.drinkName.map)
     </main>
   );
 }
---
+++ components/drinkSelected.tsx
@@ -0,0 +1,7 @@
+export default function showDrink( {name} ) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
